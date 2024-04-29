3c637e20bd8c2dddcd6f3841b3145bc6ba3fd2fd
Jan Zimmer <galileo@MacedoBuech.local>
2024-04-30 01:36:01 +0200

    initial earth with buttons

---
+++ src/app/stories/page.js
@@ -0,0 +1,11 @@
+// pages/index.tsx
+import ThreeScene from "../../components/ThreeScene";
+
+export default function Page() {
+  return (
+    <div>
+      <h1>Welcome to my Three.js Test scene!</h1>
+      <ThreeScene />
+    </div>
+  );
+}

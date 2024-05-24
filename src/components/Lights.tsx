26c0ba002e382aafb0d4dfd27e42970cd777cd6a
Jan Zimmer <galileo@MacedoBuech.local>
2024-05-24 23:32:51 +0200

    quick fix

--- src/components/Lights.tsx
+++ src/components/Lights.tsx
@@ -5,7 +5,7 @@ const Lights = () => {
   return (
     <>
       <ambientLight intensity={0.8} />
-      <directionalLight position={[5, 3, 5]} intensity={1.5} />>
+      <directionalLight position={[5, 3, 5]} intensity={1.5} />
       <hemisphereLight groundColor={0x080820} intensity={1} />
     </>
   );
